0d27fb7df5ef4a4c204f411b58dc9bad21cb9048
Reavanth Kanna R <133875433+ReavanthKanna@users.noreply.github.com>
2025-08-19 16:01:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
-import express from 'express';
+import app from './app.js';
 import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import userRoutes from './routes/userRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
-import cors from 'cors';
 
 dotenv.config();
-connectDB();
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
